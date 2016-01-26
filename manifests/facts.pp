aeda65d0725de5f209e9a7153e71ff8869976995
Jonathan Gray <jhg03a@acu.edu>
2016-01-26 15:22:09 -0600

    Fix for Issue #12 where factor isn't found in the cron path
    Conflicts:
    	manifests/facts.pp
    	manifests/facts/cronjob.pp

--- manifests/facts.pp
+++ manifests/facts.pp
@@ -16,9 +16,10 @@ class mcollective::facts inherits mcollective::facts::cronjob {
   # Just in case they define the variable and include the class both
   if( ! $mcollective::facts::cronjob::run_every ) {
     # Override to enable and set minutes
-    Cron['mcollective-facts'] {
-      ensure => present,
-      minute => '*/10',
+    Cron['mcollective-facts'] { 
+      ensure      => present,
+      minute      => '*/10',
+      environment => 'PATH=/bin:/usr/bin:/usr/sbin:/opt/puppetlabs/bin',
     }
   }
 }
